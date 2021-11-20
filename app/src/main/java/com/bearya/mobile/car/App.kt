e533d8a88c39826aef8f91069ccc580ad176e0b9
infinite.likelins <infinite.likelins@gmail.com>
2021-11-20 10:46:55 +0800

    delete : bugly

--- app/src/main/java/com/bearya/mobile/car/App.kt
+++ app/src/main/java/com/bearya/mobile/car/App.kt
@@ -2,13 +2,11 @@ package com.bearya.mobile.car
 
 import android.app.Application
 import com.kelin.apkUpdater.ApkUpdater
-import com.tencent.bugly.crashreport.CrashReport
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        CrashReport.initCrashReport(this, "b8e7263382", true)
         ApkUpdater.init(this,"${BuildConfig.APPLICATION_ID}.fileProvider")
     }
 
